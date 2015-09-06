19673956501f1caa72e177c6766836769128a681
Jason.Zhu <tozhuzhiqing@hotmail.com>
2015-09-06 15:53:21 +0800

    添加全排列算法实现

---
+++ src/com/jason/other/algrithm/Perm.java
@@ -0,0 +1,36 @@
+package com.jason.other.algrithm;
+
+public class Perm {
+
+	static void swap(int []array,int pos1,int pos2){
+		if(array[pos1]!=array[pos2]){
+			array[pos1] ^=array[pos2];
+			array[pos2] ^=array[pos1];
+			array[pos1] ^=array[pos2];
+		}
+	}
+	
+	static void perm(int []array, int begin, int end){
+		int i,j;
+		if(begin==end){
+			//一个排序形成
+            for(i=0;i<=end;i++){  
+                System.out.print(array[i]+" ");  
+            }  
+            System.out.println(); 
+            return;
+		}
+		else{
+			for(j=begin;j<=end;j++){
+				swap(array,begin,j);
+				perm(array,begin+1,end);
+				swap(array,begin,j);
+			}
+		}
+	}
+	
+	public static void main(String []args){
+		int [] array=new int []{1,2,3};
+		perm(array,0,array.length-1);
+	}
+}
